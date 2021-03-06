c303adf6f3c10b3cd18ef5f956e3d1a97537cc51
2183612059 <1025997470@qq.com>
2021-03-06 17:34:34 +0800

    增加router.js，控制vue-router路由

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from '@/extend/vueFactory'
-
+import router from "@/util/router";
 import App from './App.vue'
 
 new Vue({
     render: h => h(App),
+    router
 }).$mount('#app')
---
+++ src/util/router.js
@@ -0,0 +1,11 @@
+import Login from "@/components/frame/Login";
+import VueRouter from "vue-router";
+
+const routes = [
+    { path: '/', component: Login }
+];
+const router = new VueRouter({
+    routes
+})
+
+export default router;
