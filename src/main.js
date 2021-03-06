11fff0dddfe5dd4b01ef0e8c69c98221f5ef33ac
2183612059 <1025997470@qq.com>
2021-03-06 17:40:42 +0800

    router.js换到extend包中，明确：组件中会用上的工具，放util包里，而类似vue-router、elementui的库，在extend包里

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from '@/extend/vueFactory'
-import router from "@/util/router";
-import App from './App.vue'
+import router from "@/extend/router";
+import App from './App'
 
 new Vue({
     render: h => h(App),
