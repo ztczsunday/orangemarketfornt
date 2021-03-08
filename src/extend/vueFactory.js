b69a439b88838b63adb014ffaa01c0e4daa0c8a4
2183612059 <1025997470@qq.com>
2021-03-08 13:06:10 +0800

    引入Vuex

--- src/extend/vueFactory.js
+++ src/extend/vueFactory.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import "@/extend/inclueEui";
 import "@/extend/includeAnt";
 /* 设置不是生产环境 */
 Vue.config.productionTip = false;
+/* 设置引入Vuex */
+Vue.use(Vuex);
 
 export default Vue;
