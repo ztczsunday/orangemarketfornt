6293e18b366cee3db51ca9bdccc2d336be2fb1ed
2183612059 <1025997470@qq.com>
2021-03-10 16:53:11 +0800

    完善了顶部栏

--- src/components/subcomponents/PageHeader.vue
+++ src/components/subcomponents/PageHeader.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <VanNavBar
-        v-if="!(title === '' || title === '商城页面')"
+        v-if="shouldNavbar"
         :title="title"
         left-arrow
         @click-left="$router.go(-1)"
         @click-right="() => {}"
     />
+    <van-search
+        v-else
+        v-model="value"
+        shape="round"
+        clearable
+        show-action
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+    >
+      <template #action>
+        <div @click="onSearch"><AIcon type="search" /></div>
+      </template>
+    </van-search>
   </div>
 </template>
 
@@ -17,16 +30,25 @@ export default {
     this.$store.subscribe(mutation => {
       if (mutation.type === 'changeTitle') {
         this.title = mutation.payload;
+        this.shouldNavbar = this.title !== '' && this.title !== '商城页面';
       }
     });
+    this.shouldNavbar = document.title !== '' && document.title !== '商城页面';
+    this.title = document.title;
   },
   data() {
     return {
-      title: ''
+      title: '',
+      value: '',
+      shouldNavbar: false
     }
   },
-  computed: {},
-  methods: {}
+  computed: {
+  },
+  methods: {
+    onSearch() {
+    }
+  }
 }
 </script>
 
--- src/extend/router.js
+++ src/extend/router.js
@@ -23,5 +23,4 @@ router.beforeEach((to, from, next) => {
     next();
 });
 
-
 export default router;
