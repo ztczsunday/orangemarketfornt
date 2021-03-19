6f907bb0b10b42dfac56c7924a6b69aa919149dd
2183612059 <1025997470@qq.com>
2021-03-19 16:26:22 +0800

    history交接

--- src/components/frame/Histories.vue
+++ src/components/frame/Histories.vue
@@ -11,6 +11,7 @@
                 :desc="item.lastBrowserDate"
                 :thumb="item.thumb"
                 :title="item.title"
+                @click="$router.push(`/?cid=${item.cid}`)"
       />
     </van-list>
   </div>
@@ -18,26 +19,50 @@
 
 <script>
 class History {
-  constructor(title, lastBrowserDate, thumb) {
+  constructor(title, lastBrowserDate, thumb, cid) {
     this.title = title;
     this.lastBrowserDate = lastBrowserDate;
     this.thumb = thumb;
+    this.cid = cid;
   }
 }
 
 export default {
   name: "Histories",
-  mounted() {
+  created() {
   },
   data() {
     return {
+      /* 表示是否Loading */
       loading: null,
-      finished: true,
-      histories: new Array(100).fill(new History("Ipad Pro", "0", "https://img01.yzcdn.cn/vant/ipad.jpeg"))
+      /* 表示是否已经到底 */
+      finished: false,
+      /* 现在最新的是第几页 */
+      nowPage: 0,
+      /* 一页大小 */
+      pageSize: 10,
+      /* 存储Histories */
+      histories: []
     }
   },
   methods: {
-    onLoad() {
+    async onLoad() {
+      const { $ } = await import("@/util/ajax");
+      const formData = new FormData();
+      formData.append("pages", this.nowPage);
+      formData.append("pageSize", this.pageSize);
+      const result = await $.get('/histories');
+      const page = result.data.information;
+      [].push.apply(this.histories, page.records.map(
+          record => new History(
+              record.commodityName,
+              `${record.lastBrowserDate[0]}年${record.lastBrowserDate[1]}月${record.lastBrowserDate[2]}日`,
+              record['mainIcon'],
+              record.cid
+          )
+      ));
+      this.nowPage++;
+      this.finished = this.nowPage >= page.total / page.size;
     }
   }
 }
