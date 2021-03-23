bdd24f1a641e5e70f98cf8330b99a748df03e00b
2183612059 <1025997470@qq.com>
2021-03-23 14:32:07 +0800

    代码整体格式化

--- src/components/frame/BuildGoods.vue
+++ src/components/frame/BuildGoods.vue
@@ -2,100 +2,100 @@
   <VanForm @submit="onSubmit">
     <VanField
         v-model="goodsName"
-        name="goodsName"
-        label="商品名"
-        placeholder="商品名"
         :rules="[{ required: true, message: '请填写商品名' }]"
+        label="商品名"
+        name="goodsName"
+        placeholder="商品名"
     />
     <VanField
         v-model="fieldValue"
-        name="label"
         is-link
-        readonly
         label="分类"
+        name="label"
         placeholder="请选择您的商品分类"
+        readonly
         @click="show = true"
     />
-    <VanPopup v-model="show" round position="bottom">
+    <VanPopup v-model="show" position="bottom" round>
       <VanCascader
           v-model="cascaderValue"
-          title="请选择商品分类"
           :options="options"
+          title="请选择商品分类"
           @close="show = false"
           @finish="onFinish"
       />
     </VanPopup>
-    <VanField name="uploader" label="商品图片上传">
+    <VanField label="商品图片上传" name="uploader">
       <template #input>
-        <VanUploader v-model="uploader" multiple
-                     name="Uploader"
+        <VanUploader v-model="uploader" :after-read="afterReadGoods"
+                     :before-delete="upLoaderDelete"
+                     :before-read="beforeRead"
                      :max-count="5"
-                     :before-read="beforeRead"
-                     :after-read="afterReadGoods"
-                     :before-delete = "upLoaderDelete"/>
+                     multiple
+                     name="Uploader"/>
       </template>
     </VanField>
-    <VanField name="uploaderSubDefault" label="商品默认小图标上传">
+    <VanField label="商品默认小图标上传" name="uploaderSubDefault">
       <template #input>
-        <VanUploader v-model="uploaderSubDefault" multiple
-                     name = "uploaderSubDefault"
+        <VanUploader v-model="uploaderSubDefault" :after-read="afterReadGoods"
+                     :before-delete="upLoaderDelete"
+                     :before-read="beforeRead"
                      :max-count="1"
-                     :before-read="beforeRead"
-                     :after-read="afterReadGoods"
-                     :before-delete="upLoaderDelete"/>
+                     multiple
+                     name="uploaderSubDefault"/>
       </template>
     </VanField>
-    <VanField name="uploaderDetail" label="商品详情">
+    <VanField label="商品详情" name="uploaderDetail">
       <template #input>
-        <VanUploader v-model="uploaderDetail" multiple
-                     name="UploaderDetail"
-                     :max-count="10"
+        <VanUploader v-model="uploaderDetail" :after-read="afterReadGoods"
+                     :before-delete="upLoaderDelete"
                      :before-read="beforeRead"
-                     :after-read="afterReadGoods"
-                     :before-delete="upLoaderDelete"/>
+                     :max-count="10"
+                     multiple
+                     name="UploaderDetail"/>
       </template>
     </VanField>
 
     <div v-for="(item,i) in typeList" :key="i">
-      <VanCell is-link @click="showPopup(i)">{{item.subName}}</VanCell>
-      <VanPopup v-model="showSub[i]" position="bottom" height="700px" closeable round :style="{height : '70%'}">
+      <VanCell is-link @click="showPopup(i)">{{ item.subName }}</VanCell>
+      <VanPopup v-model="showSub[i]" :style="{height : '70%'}" closeable height="700px" position="bottom" round>
         <div class="popBlock">
           您的品类
         </div>
         <VanForm @submit="upDateSub(i)">
           <VanField
               v-model="typeList[i].subName"
-              name="subTypeName"
-              label="子种类名"
-              placeholder="子种类名"
               :rules="[{ required: true, message: '请填写子种类名' }]"
+              label="子种类名"
+              name="subTypeName"
+              placeholder="子种类名"
           />
           <VanField
               v-model="typeList[i].subPrice"
-              name="subTypePrice"
-              label="子种类单价"
-              placeholder="子种类单价"
               :rules="[{ required: true, message: '请填写子种类单价' }]"
+              label="子种类单价"
+              name="subTypePrice"
+              placeholder="子种类单价"
           />
           <VanField
               v-model="typeList[i].subStock"
-              name="subTypeStock"
-              label="子种类库存"
-              placeholder="子种类库存"
               :rules="[{ required: true, message: '请填写子种类库存' }]"
+              label="子种类库存"
+              name="subTypeStock"
+              placeholder="子种类库存"
           />
-          <VanField name="uploaderSub" label="种类图标上传">
+          <VanField label="种类图标上传" name="uploaderSub">
             <template #input>
-              <VanUploader v-model="typeList[i].subPic" multiple
-                           name = i
+              <VanUploader v-model="typeList[i].subPic" :before-read="beforeRead"
                            :max-count="1"
-                           :before-read="beforeRead"/>
+                           multiple
+                           name="i"/>
             </template>
           </VanField>
           <div style="margin: 16px;">
-            <VanButton round block type="info" native-type="submit">确认</VanButton>
+            <VanButton block native-type="submit" round type="info">确认</VanButton>
             <ARow :style="{'height' : '10px'}"></ARow>
-            <VanButton round block type="danger" @click="deleteSub(i)">删除</VanButton>
+            <VanButton block round type="danger" @click="deleteSub(i)">删除</VanButton>
           </div>
         </VanForm>
       </VanPopup>
@@ -103,45 +103,45 @@
 
 
     <VanCell is-link @click="showPopupAdd">新建子类</VanCell>
-    <VanPopup v-model="showAdd" position="bottom" closeable round :style="{height : '70%'}">
+    <VanPopup v-model="showAdd" :style="{height : '70%'}" closeable position="bottom" round>
       <div class="popBlock">
         新的子种类
       </div>
       <VanForm @submit="addSub">
         <VanField
             v-model="subTypeName"
-            name="subTypeName"
-            label="子种类名"
-            placeholder="子种类名"
             :rules="[{ required: true, message: '请填写子种类名' }]"
+            label="子种类名"
+            name="subTypeName"
+            placeholder="子种类名"
         />
         <VanField
             v-model="subTypePrice"
-            name="subTypePrice"
-            label="子种类单价"
-            placeholder="子种类单价"
             :rules="[{ required: true, message: '请填写子种类单价' }]"
+            label="子种类单价"
+            name="subTypePrice"
+            placeholder="子种类单价"
         />
         <VanField
             v-model="subTypeStock"
-            name="subTypeStock"
-            label="子种类库存"
-            placeholder="子种类库存"
             :rules="[{ required: true, message: '请填写子种类库存' }]"
+            label="子种类库存"
+            name="subTypeStock"
+            placeholder="子种类库存"
         />
-        <VanField name="uploaderSub" label="种类图标上传">
+        <VanField label="种类图标上传" name="uploaderSub">
           <template #input>
-            <VanUploader v-model="uploaderSub" multiple :max-count="1"
-                         :before-read="beforeRead"/>
+            <VanUploader v-model="uploaderSub" :before-read="beforeRead" :max-count="1"
+                         multiple/>
           </template>
         </VanField>
         <div style="margin: 16px;">
-          <VanButton round block type="info" native-type="submit">确认</VanButton>
+          <VanButton block native-type="submit" round type="info">确认</VanButton>
         </div>
       </VanForm>
     </VanPopup>
     <div style="margin: 16px;">
-      <VanButton round block type="info" native-type="submit">提交</VanButton>
+      <VanButton block native-type="submit" round type="info">提交</VanButton>
     </div>
   </VanForm>
 </template>
@@ -152,12 +152,12 @@ import { Toast } from 'vant';
 export default {
   name: "BuildGoods",
   async created() {
-    const {$} = await  import('@/util/ajax');
+    const { $ } = await import('@/util/ajax');
     const result = await $.get('/commodity/labels')
-    for(let i = 0; i < result.data.information.length; i++){
+    for (let i = 0; i < result.data.information.length; i++) {
       this.options.push({
-        text : result.data.information[i].itemName,
-        value : result.data.information[i].recordId,
+        text: result.data.information[i].itemName,
+        value: result.data.information[i].recordId,
       })
     }
     console.log(this.options)
@@ -168,51 +168,49 @@ export default {
       label: [],
       //存商品图片
       uploader: [],
-      uploaderName :[],
+      uploaderName: [],
       //存该种类下的商品图标(临时的)
-      uploaderSub:[],
-      uploaderSubName : null,
+      uploaderSub: [],
+      uploaderSubName: null,
       //存默认的商品图标
-      uploaderSubDefault : [],
-      uploaderSubDefaultName : null,
+      uploaderSubDefault: [],
+      uploaderSubDefaultName: null,
       //商品详情
       uploaderDetail: [],
-      uploaderDetailName:[],
+      uploaderDetailName: [],
 
-      goodsName : null,
+      goodsName: null,
       show: false,
       fieldValue: '',
       cascaderValue: '',
-      showSub : [false],
-      showAdd : false,
-      subTypeName : null,
-      subTypePrice : null,
-      subTypeStock : null,
+      showSub: [false],
+      showAdd: false,
+      subTypeName: null,
+      subTypePrice: null,
+      subTypeStock: null,
       options: [],
-      typeList:[],
-      postList:[],
+      typeList: [],
+      postList: [],
     };
   },
   methods: {
     async onSubmit() {
-      if(this.uploader.length === 0 || this.uploaderDetail.length === 0 || this.uploaderSubDefault.length === 0){
+      if (this.uploader.length === 0 || this.uploaderDetail.length === 0 || this.uploaderSubDefault.length === 0) {
         Toast("缺少图片");
-      }
-      else if(this.typeList.length === 0){
+      } else if (this.typeList.length === 0) {
         Toast("至少设置一个种类");
-      }
-      else{
+      } else {
         const config = {
           headers: { "Content-Type": "application/json;charset=UTF-8" }
         };
-        const {$} = await  import('@/util/ajax');
-        for(let i = 0; i < this.options.length; i++ ){
-          if(this.options[i].text === this.fieldValue){
+        const { $ } = await import('@/util/ajax');
+        for (let i = 0; i < this.options.length; i++) {
+          if (this.options[i].text === this.fieldValue) {
             this.label.push(this.options[i].value)
           }
         }
 
-        for(let i = 0; i < this.typeList.length; i++){
+        for (let i = 0; i < this.typeList.length; i++) {
           this.postList.push(
               {
                 "cid": 0,
@@ -225,16 +223,16 @@ export default {
               }
           )
         }
-        const test ={
-          "commodityDetails" : this.uploaderDetailName,
-          "commodityName" : this.goodsName,
-          "labelId" : this.label,
-          "mainIcon":this.uploaderSubDefaultName,
-          "mainIcons":this.uploaderName,
-          "subCommodity":this.postList
+        const test = {
+          "commodityDetails": this.uploaderDetailName,
+          "commodityName": this.goodsName,
+          "labelId": this.label,
+          "mainIcon": this.uploaderSubDefaultName,
+          "mainIcons": this.uploaderName,
+          "subCommodity": this.postList
         }
         console.log(test)
-        const result = await $.post('/commodity', test,config);
+        const result = await $.post('/commodity', test, config);
         console.log(result);
       }
     },
@@ -249,32 +247,31 @@ export default {
       }
       return true;
     },
-    showPopup(i){
+    showPopup(i) {
       this.showSub[i] = true;
       this.showAdd = true;
-      this.showAdd=false;
+      this.showAdd = false;
     },
-    showPopupAdd(){
+    showPopupAdd() {
       this.showAdd = true;
     },
-    async addSub(values){
-      if(this.uploaderSub.length===0){
+    async addSub(values) {
+      if (this.uploaderSub.length === 0) {
         Toast("至少上传一个图片")
-      }
-      else{
+      } else {
         const config = {
-          headers: { "Content-Type": "multipart/form-data;boundary="+new Date().getTime() }
+          headers: { "Content-Type": "multipart/form-data;boundary=" + new Date().getTime() }
         };
-        const {$} = await  import('@/util/ajax');
+        const { $ } = await import('@/util/ajax');
         const formData = new FormData();
         formData.append('file', this.uploaderSub[0].file);
-        const result =await $.post('/upload', formData, config);
+        const result = await $.post('/upload', formData, config);
         const sub = {
-          subName : values.subTypeName,
-          subPrice : parseInt(values.subTypePrice),
-          subStock : parseInt(values.subTypeStock),
-          subPic : this.uploaderSub,
-          subPicName : result.data.information,
+          subName: values.subTypeName,
+          subPrice: parseInt(values.subTypePrice),
+          subStock: parseInt(values.subTypeStock),
+          subPic: this.uploaderSub,
+          subPicName: result.data.information,
         }
         this.showSub.push(false);
         this.typeList.push(sub);
@@ -285,57 +282,53 @@ export default {
         this.subTypeName = null;
       }
     },
-    async upDateSub(i){
+    async upDateSub(i) {
       const config = {
-        headers: { "Content-Type": "multipart/form-data;boundary="+new Date().getTime() }
+        headers: { "Content-Type": "multipart/form-data;boundary=" + new Date().getTime() }
       };
-      const {$} = await  import('@/util/ajax');
+      const { $ } = await import('@/util/ajax');
       const formData = new FormData();
       formData.append('file', this.typeList[i].subPic[0].file);
       console.log(this.typeList[i].subPic[0].file)
-      const result =await $.post('/upload', formData, config);
+      const result = await $.post('/upload', formData, config);
       console.log(result);
-      this.typeList[i].subPicName=result.data.information;
+      this.typeList[i].subPicName = result.data.information;
       Toast("修改成功");
       this.showSub[i] = false;
       this.showAdd = true;
       this.showAdd = false;
       console.log(this.typeList[i].subPicName);
     },
-    async afterReadGoods(file,detail){
+    async afterReadGoods(file, detail) {
       const config = {
-        headers: { "Content-Type": "multipart/form-data;boundary="+new Date().getTime() }
+        headers: { "Content-Type": "multipart/form-data;boundary=" + new Date().getTime() }
       };
-      const {$} = await  import('@/util/ajax');
+      const { $ } = await import('@/util/ajax');
       const formData = new FormData();
       formData.append('file', file.file);
-      const result =await $.post('/upload', formData, config);
-      if(detail.name === "Uploader"){
+      const result = await $.post('/upload', formData, config);
+      if (detail.name === "Uploader") {
         this.uploaderName.push(result.data.information);
-      }
-      else if(detail.name === "UploaderDetail"){
+      } else if (detail.name === "UploaderDetail") {
         this.uploaderDetailName.push(result.data.information)
-      }
-      else if(detail.name === "uploaderSubDefault"){
+      } else if (detail.name === "uploaderSubDefault") {
         this.uploaderSubDefaultName = result.data.information
       }
       console.log(this.uploaderName);
     },
-    upLoaderDelete(detail){
-      if(detail.name === "Uploader"){
-        this.uploaderName.splice(detail.index,1);
-      }
-      else if(detail.name === "UploaderDetail"){
-        this.uploaderDetailName.splice(detail.index,1);
-      }
-      else if(detail.name === "uploaderSubDefault"){
+    upLoaderDelete(detail) {
+      if (detail.name === "Uploader") {
+        this.uploaderName.splice(detail.index, 1);
+      } else if (detail.name === "UploaderDetail") {
+        this.uploaderDetailName.splice(detail.index, 1);
+      } else if (detail.name === "uploaderSubDefault") {
         this.uploaderSubDefaultName = null;
       }
       console.log(this.uploaderName)
       return Promise;
     },
-    deleteSub(i){
-      this.typeList.splice(i,1);
+    deleteSub(i) {
+      this.typeList.splice(i, 1);
       Toast("删除成功");
       this.showSub[i] = false;
       this.showAdd = true;
@@ -346,7 +339,7 @@ export default {
 </script>
 
 <style scoped>
-.popBlock{
+.popBlock {
   padding-top: 20px;
   padding-left: 20px;
   font-weight: bold;
